67adc08ce2170add0901fb6f797d812109377d2e
kingfest19 <blesslub10@gmail.com>
2026-01-17 20:54:43 -0800

    Fix whitenoise build error: catch ValueError and MissingFileError

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -4,11 +4,13 @@ class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
     manifest_strict = False
 
     def __init__(self, *args, **kwargs):
+        # Explicitly pass manifest_strict=False to the parent constructor
+        kwargs['manifest_strict'] = False
         super().__init__(*args, **kwargs)
         self.manifest_strict = False
 
     def hashed_name(self, name, content=None, filename=None):
         try:
             return super().hashed_name(name, content, filename)
-        except MissingFileError:
+        except (MissingFileError, ValueError):
             return name
